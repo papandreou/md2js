19983780697a1f0fe2d238507b1e282294d8ff29
Andreas Lind <andreas@one.com>
2015-08-15 08:45:09 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var md2js = require('./lib/md2js');
+
+require.extensions['.md'] = function (module, fileName) {
+    module._compile(md2js(fs.readFileSync(fileName, 'utf-8'), fileName));
+};
