5fe60c7842f1ff4f86649fb1ba99a909fa0ab9ec
Andreas Lind <andreas@one.com>
2015-08-20 23:11:38 +0200

    Fix mapping of errors occurring in the generated code.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var md2js = require('./lib/md2js');
 
 require.extensions['.md'] = function (module, fileName) {
-    module._compile(md2js(fs.readFileSync(fileName, 'utf-8'), fileName));
+    module._compile(md2js(fs.readFileSync(fileName, 'utf-8'), fileName), fileName);
 };
